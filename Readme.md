73d11749c26b9127b452c59aed8919a57340cc86
dxBot <DevExpressExampleBot>
2018-02-25 22:13:13 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to adjust DevExpress ASP.NET MVC extensions when opening page in IE Compatibility Mode
+
+
+<p>Starting with version v2014 vol 2 (14.2), it is much easier to specify the target IE Compatibility Version with the help of a new <a href="https://documentation.devexpress.com/#AspNet/CustomDocument17771">ieCompatibilityVersion</a> configuration option.<br /><br />This example is an illustration of the KA18646: <a href="https://www.devexpress.com/Support/Center/p/KA18646">How to adjust DevExpress ASP.NET controls when opening a page in the Internet Explorer Compatibility Mode</a> KB Article. Refer to the Article for an explanation.</p>
+
+<br/>
+
+
---
+++ VB/MvcGridView_example/Controllers/HomeController.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web.Mvc
+Imports DevExpress.Web.ASPxClasses.Internal
+Imports DevExpress.Web.ASPxClasses
+
+Namespace MvcGridView_example.Controllers
+	Public Class HomeController
+		Inherits Controller
+		Public Function Index() As ActionResult
+			Return View()
+		End Function
+		Protected Overrides Sub OnActionExecuted(ByVal filterContext As ActionExecutedContext)
+			MyBase.OnActionExecuted(filterContext)
+			If RenderUtils.Browser.IsIE Then
+				ASPxWebControl.SetIECompatibilityMode(7)
+				Dim IEVersion = 7
+				ViewData("MetaContent") = String.Format("<meta http-equiv=""X-UA-Compatible"" content=""IE={0}"" />", IEVersion)
+			End If
+		End Sub
+
+	End Class
+End Namespace
