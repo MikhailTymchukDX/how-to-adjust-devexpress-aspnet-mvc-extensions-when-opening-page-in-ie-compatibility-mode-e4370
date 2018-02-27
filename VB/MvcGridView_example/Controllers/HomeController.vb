330558c4998a9702d5be918d48019eb4e9f53f25
DevExpressExampleBot <fakeEmail>
2018-02-27 10:52:32 +0300

    Source auto update [skip ci]

--- VB/MvcGridView_example/Controllers/HomeController.vb
+++ VB/MvcGridView_example/Controllers/HomeController.vb
@@ -1,25 +1,11 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Web.Mvc
-Imports DevExpress.Web.ASPxClasses.Internal
-Imports DevExpress.Web.ASPxClasses
+﻿Imports System.Web.Mvc
 
 Namespace MvcGridView_example.Controllers
-	Public Class HomeController
-		Inherits Controller
-		Public Function Index() As ActionResult
-			Return View()
-		End Function
-		Protected Overrides Sub OnActionExecuted(ByVal filterContext As ActionExecutedContext)
-			MyBase.OnActionExecuted(filterContext)
-			If RenderUtils.Browser.IsIE Then
-				ASPxWebControl.SetIECompatibilityMode(7)
-				Dim IEVersion = 7
-				ViewData("MetaContent") = String.Format("<meta http-equiv=""X-UA-Compatible"" content=""IE={0}"" />", IEVersion)
-			End If
-		End Sub
+    Public Class HomeController
+        Inherits Controller
 
-	End Class
+        Public Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
 End Namespace
--- VB/MvcGridView_example/Global.asax.vb
+++ VB/MvcGridView_example/Global.asax.vb
@@ -1,34 +1,34 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Web
+﻿Imports System
 Imports System.Web.Mvc
 Imports System.Web.Routing
-Imports DevExpress.Web.ASPxClasses.Internal
 
 Namespace MvcGridView_example
-	' Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	' visit http://go.microsoft.com/?LinkId=9394801
+    ' Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    ' visit http://go.microsoft.com/?LinkId=9394801
 
-	Public Class MvcApplication
-		Inherits System.Web.HttpApplication
-		Public Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
-			filters.Add(New HandleErrorAttribute())
-		End Sub
+    Public Class MvcApplication
+        Inherits System.Web.HttpApplication
 
-		Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+        Public Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
+            filters.Add(New HandleErrorAttribute())
+        End Sub
 
-			routes.MapRoute("Default", "{controller}/{action}/{id}", New With {Key .controller = "Home", Key .action = "Index", Key .id = UrlParameter.Optional})
+        Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
 
-		End Sub
+            routes.MapRoute("Default", "{controller}/{action}/{id}", New With {Key .controller = "Home", Key .action = "Index", Key .id = UrlParameter.Optional}) ' Parameter defaults -  URL with parameters -  Route name
 
-		Protected Sub Application_Start()
-			AreaRegistration.RegisterAllAreas()
+        End Sub
 
-			RegisterGlobalFilters(GlobalFilters.Filters)
-			RegisterRoutes(RouteTable.Routes)
-		End Sub
-	End Class
+        Protected Sub Application_Start()
+            AreaRegistration.RegisterAllAreas()
+
+            RegisterGlobalFilters(GlobalFilters.Filters)
+            RegisterRoutes(RouteTable.Routes)
+        End Sub
+
+        Protected Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
+            DevExpress.Web.ASPxClasses.ASPxWebControl.SetIECompatibilityMode(7)
+        End Sub
+    End Class
 End Namespace
